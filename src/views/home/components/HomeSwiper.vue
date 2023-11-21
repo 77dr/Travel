87bb3b038a26b123c1e67cafcacf03659292a2ad
77 <1420274699@qq.com>
2023-11-21 17:09:58 +0800

    feat: add swiper

---
+++ src/views/home/components/HomeSwiper.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  'list': Array
+})
+const list = props.list
+const showSwiper = computed(() => {
+  return props.list.length
+})
+</script>
+
+<template>
+  <div class="swiper">
+    <van-swipe
+      v-if="showSwiper"
+      indicator-color="#fff"
+    >
+      <van-swipe-item
+        v-for="item of list"
+        :key="item.id"
+      >
+        <img
+          :src="item.imgUrl"
+          class="swiper-img"
+        />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+  // 样式穿透
+  // .swiper >>> .van-swipe__indicator--active
+  //   background-color: #fff
+  .swiper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    // height: 31.25vw // 浏览器兼容？
+    .swiper-img
+      width: 100%
+</style>
