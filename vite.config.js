58c9fe5f66167bbbc85aed03be178105fe497cb3
77 <1420274699@qq.com>
2023-12-26 10:58:13 +0800

    Fix: Introducing CSS errors

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 
+import path from 'path'
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -27,6 +31,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       'styles': fileURLToPath(new URL('./src/assets/styles', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      stylus: {
+        additionalData: `@import "${path.resolve(__dirname, 'src/assets/styles/varibles.styl')}"`
+      }
     }
   }
 })
